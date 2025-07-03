0863e948efb7b241f125d4f4e34a3655b028c122
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-07-03 21:25:08 +0530

    refactor: replace error throw with console error for push failures

--- src/git.js
+++ src/git.js
@@ -42,11 +42,14 @@ export async function commitAndPush(repoPath, apiKey){
         await git.push();
         console.log(`Committed and pushed ${repoPath}`);
     } catch (e){
+        if(e.message.includes("No configured push destination.")){
+            console.log("The repo doesn't have an origin");
+        }
         if(e.message.includes("Permission denied (publickey)")){
             console.log("SSH Authentication failed \nAdd you public key to github \n run gitdrip setup")
 
         } else {
-        throw new error('Failed to push: ', e.message);
+        console.error('Failed to push: ', e.message);
         }
     }
 }
